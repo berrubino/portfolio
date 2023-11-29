f63d76d1b1ce9156618207df23851ab5ee3ed33e
berrubino <berni.rubino@gmail.com>
2023-11-29 15:03:57 -0300

    feat:project card first stules created

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import ProjectCards from "../../commons/pojects-cards/ProjectsCards";
 import "../Projects/projects.css";
 
 function Projects() {
-  return <div className="projects-container">Projects</div>;
+  return (
+    <div className="projects-container">
+      <ProjectCards />
+    </div>
+  );
 }
 
 export default Projects;
